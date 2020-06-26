068a19dd37f05f436c527bfb765abe8e5e695258
kevinpaulJose <bkevin1999@gmail.com>
2020-06-26 17:54:42 +0530

    send email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,39 +1,58 @@
 import React from 'react';
-import { Card, Text } from 'react-native-elements';
+import { Card, Text, Button, Icon } from 'react-native-elements';
 import { StyleSheet, ScrollView } from 'react-native';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
-export default function Contact() {
-    return(
-        <ScrollView style={styles.background}>
- 
-            <Animatable.View animation='fadeInDown' duration={500} delay={100}>
-                <Card
-                    title="Contact Information"
-                >
-                    <Text style={styles.addressInfo}>
-                        121, Clear Water Bay Road
-                    </Text>
-                    <Text style={styles.addressInfo}>
-                    Clear Water Bay, Kowloon
-                    </Text>
-                    <Text style={styles.addressInfo}>
-                    HONG KONG
-                    </Text>
-                    <Text style={styles.addressInfo}>
-                    Tel: +852 1234 5678
-                    </Text>
-                    <Text style={styles.addressInfo}>
-                    Fax: +852 8765 4321
-                    </Text>
-                    <Text style={styles.addressInfo}>
-                        Email:confusion@food.net
-                    </Text>
+class Contact extends React.Component {
 
-                </Card>  
-            </Animatable.View>      
-        </ScrollView>
-    );
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@test.com'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern.'
+        });
+    }
+
+    render() {
+        return(
+            <ScrollView style={styles.background}>
+     
+                <Animatable.View animation='fadeInDown' duration={500} delay={100}>
+                    <Card
+                        title="Contact Information"
+                    >
+                        <Text style={styles.addressInfo}>
+                            121, Clear Water Bay Road
+                        </Text>
+                        <Text style={styles.addressInfo}>
+                        Clear Water Bay, Kowloon
+                        </Text>
+                        <Text style={styles.addressInfo}>
+                        HONG KONG
+                        </Text>
+                        <Text style={styles.addressInfo}>
+                        Tel: +852 1234 5678
+                        </Text>
+                        <Text style={styles.addressInfo}>
+                        Fax: +852 8765 4321
+                        </Text>
+                        <Text style={styles.addressInfo}>
+                            Email:confusion@food.net
+                        </Text>
+                        <Button
+                        title="Send Email"
+                        buttonStyle={{backgroundColor: "#512DA8"}}
+                        icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                        onPress={this.sendMail}
+                        />
+    
+                    </Card>  
+                </Animatable.View>      
+            </ScrollView>
+        );
+    }
+
 }
 
 const styles = StyleSheet.create({
@@ -46,3 +65,5 @@ const styles = StyleSheet.create({
         backgroundColor: "#E9E9EF"
     }
 })
+
+export default Contact;
